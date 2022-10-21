a839928b7c6c5db3653ac2b2c5026b7a2df41cb8
Nathan Freeman <nathanlfreeman@gmail.com>
2022-10-21 01:48:35 -0400

    Try HashRouter to get React Router to work in Github Pages

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}
-    >
-    <App />
-    </Auth0Provider>
+    <HashRouter>
+
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        redirectUri={window.location.origin}
+        >
+      <App />
+      </Auth0Provider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
